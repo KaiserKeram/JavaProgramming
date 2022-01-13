594f92e72f56bda2b78a9d0cbe53c05d6ea4fab2
kaiser <ulug0829@gmail.com>
2022-01-13 10:55:48 -0500

    day38:Inheritance

---
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -0,0 +1,20 @@
+package day38_Inheritance.carTask;
+
+public class Toyota extends  Car{
+
+    public Toyota(String model, String color, int year, int miles, double price) {
+        super("Toyota", model, color, year, miles, price);
+    }
+
+    public void reliable(){
+        System.out.println(brand + " " + model + " is reliable");
+    }
+
+
+}
+
+/*
+Toyota:
+                extra methods:
+                    reliable()
+ */
