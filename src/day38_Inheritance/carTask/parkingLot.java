001f838e4bc569861ffa4298ce2ee1962105dda9
kaiser <ulug0829@gmail.com>
2022-01-13 11:48:59 -0500

    day38:Inheritance

---
+++ src/day38_Inheritance/carTask/parkingLot.java
@@ -0,0 +1,12 @@
+package day38_Inheritance.carTask;
+
+public class parkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry", "Black", 2016,12000,20000);
+        Tesla tesla = new Tesla("Model S","red",2021,2000,45000);
+        BMW bmw = new BMW("X5","Black",2019,3000,50000);
+
+    }
+}
