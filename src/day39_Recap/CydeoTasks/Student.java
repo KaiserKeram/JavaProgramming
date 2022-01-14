cea2ee4af1efe8aa025bdba33602ef9372e11284
kaiser <ulug0829@gmail.com>
2022-01-14 16:26:46 -0500

    day39_Recap

--- src/day39_Recap/CydeoTasks/Student.java
+++ src/day39_Recap/CydeoTasks/Student.java
@@ -57,10 +57,5 @@ public class Student extends Person{
             Methods:
                 study()
 
-    7. Create a class named Cydeo:
-            create objects of Tester, Developer,Teacher, Student
 
-            test all the functions of each objects
-
-            Analyze the relationships between the classes
  */
