7655d8117dac0c3e60736e9a405ef2acef390eb6
kaiser <ulug0829@gmail.com>
2022-01-14 16:18:40 -0500

    day39_Recap

--- src/day39_Recap/CydeoTasks/Person.java
+++ src/day39_Recap/CydeoTasks/Person.java
@@ -86,43 +86,9 @@ CydeoTask:
 
 
 
-    3. Create a sub class of Employee named Tester
-
-            Override the work method
-
-            Extra methods:
-                createTicket()
-
-    4. Create a sub class of Employee named Developer
-
-            Override the work method
-
-            Extra methods:
-                fixingBugs()
-
-    5. Create a sub class of Employee named Teacher
-
-            Override the work method
 
 
-    6. Create a sub class of Person named Student
 
-            Extra variables:
-                studentId, fieldOfStudy
 
-            Encapsulate all the fields
 
-            Add a constructor to set all the fields
-
-            Encapsulate all the fields
-
-            Methods:
-                study()
-
-    7. Create a class named Cydeo:
-            create objects of Tester, Developer,Teacher, Student
-
-            test all the functions of each objects
-
-            Analyze the relationships between the classes
  */
