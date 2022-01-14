716d46ff00e5c5ec15f9c7d65fcfbd01d89fe7a3
kaiser <ulug0829@gmail.com>
2022-01-14 10:57:01 -0500

    day39_Recap

--- src/day39_Recap/Shape/Shape.java
+++ src/day39_Recap/Shape/Shape.java
@@ -1,4 +1,34 @@
 package day39_Recap.Shape;
 
 public class Shape {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name == null){
+            System.out.println("Name can not be null");
+            System.exit(1); // 1 : something went wrong
+        }
+
+        if(name.isEmpty() || name.isBlank()){
+            System.out.println("Invalid name");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public Shape(String name) {
+        setName(name);
+    }
+
+    public double area(){
+        return 0;
+    }
+
+    public double perimeter(){
+        return 0;
+    }
 }
